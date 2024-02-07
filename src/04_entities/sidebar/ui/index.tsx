90823512baad148c19d2d2b2d0f7dfb4a53ae888
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2024-02-07 08:39:09 +0300

    chapter 9 in the tutorial has been completed

--- src/04_entities/sidebar/ui/index.tsx
+++ src/04_entities/sidebar/ui/index.tsx
@@ -2,24 +2,39 @@ import { Card } from "@mantine/core";
 import { useMemo } from "react";
 import { Node } from "src/05_shared/types";
 import SidebarNode from "./sidebar-node";
-
+import { IconFolder, IconHome2 } from "@tabler/icons-react";
 
 export type SidebarProps = {
   rootDirectory?: Node;
 };
 
-export function Sidebar({rootDirectory}: SidebarProps) {
+export function Sidebar({ rootDirectory }: SidebarProps) {
   const rootChildren = useMemo(() => {
-    return rootDirectory?.children?.filter(child => child.isDirectory);
+    return rootDirectory?.children?.filter((child) => child.isDirectory);
   }, [rootDirectory]);
 
   if (!rootDirectory) return null;
 
-
   return (
     <Card shadow="sm">
-      {rootChildren?.map(child => (
-        <SidebarNode key={child.path} node={child} />
+      <SidebarNode
+        node={rootDirectory}
+        icon={<IconHome2 size={16} color="#78a136" />}
+        navProps={{
+          p: 0,
+          pl: 5,
+        }}
+      />
+      {rootChildren?.map((child) => (
+        <SidebarNode
+          key={child.path}
+          node={child}
+          icon={<IconFolder size={16} fill="#31caf9" />}
+          navProps={{
+            p: 0,
+            pl: 15,
+          }}
+        />
       ))}
     </Card>
   );
--- src/04_entities/sidebar/ui/sidebar-node.tsx
+++ src/04_entities/sidebar/ui/sidebar-node.tsx
@@ -1,13 +1,40 @@
+import { IconFolder } from "@tabler/icons-react";
 import { Node } from "src/05_shared/types";
+import classes from "./classes.module.css";
+import useFileManager from "src/05_shared/lib/hooks/useFileManager";
+import { useEffect, useState } from "react";
+import { NavLink, NavLinkProps } from "@mantine/core";
 
 export type SidebarNodeProps = {
   node: Node;
+  icon?: React.ReactNode;
+  navProps?: Partial<NavLinkProps>;
 };
 
-export default function SidebarNode({ node }: SidebarNodeProps) {
+export default function SidebarNode({ icon, node, navProps={}, }: SidebarNodeProps) {
+  const fileManager = useFileManager();
+  const [isActiveNode, setIsActiveNode] = useState(
+    node === fileManager.currentDirectoryNode
+  );
+
+  useEffect(() => {
+    setIsActiveNode(node === fileManager.currentDirectoryNode);
+  }, [fileManager.currentDirectoryNode, node]);
+
   return (
-    <>
-      <div>{node.name}</div>
-    </>
+    <NavLink
+      {...navProps}
+      active={isActiveNode}
+      label={
+        <>
+          <span className={classes["icon-wrapper"]}>{icon}</span>
+          <span>{node.name}</span>
+        </>
+      }
+    />
   );
 }
+
+SidebarNode.defaultProps = {
+  icon: <IconFolder fill="#31caf9" />,
+};
